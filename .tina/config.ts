986c18ab5591d8808a326f54ed850d6260145fe6
Alexander Prestmo <aprestmo@gmail.com>
2023-01-26 14:59:48 +0100

    Made a change

--- .tina/config.ts
+++ .tina/config.ts
@@ -1,72 +1,76 @@
-
 import { defineStaticConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineStaticConfig({
-  branch,
-  clientId: null,   // Get this from tina.io
-  token: null,      // Get this from tina.io
-  build: {
-    outputFolder: "admin",
-    publicFolder: "public",
-  },
-  media: {
-    tina: {
-      mediaRoot: "uploads",
-      publicFolder: "public",
-    },
-  },
-  schema: {
-    collections: [
-      {
-        name: "post",
-        label: "Posts",
-        path: "src/content",
-        format: 'mdx',
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-          },{
-            type: "string",
-            name: "description",
-            label: "Description",
-          },{
-            type: "image",
-            name: "heroImage",
-            label: "Hero Image",
-          },{
-            type: "datetime",
-            name: "pubDate",
-            label: "Publish Date",
-            ui: {
-              dateFormat: "DD MMMM YYYY"
-            },
-          },{
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-            templates: [
-              {
-                name: "Counter",
-                label: "Counter",
-                fields: [
-                  {
-                    name: "variableExample",
-                    label: "Variable Example",
-                    type: "string",
-                  }
-                ],
-              },
-            ],
-          }
-        ],
-      },
-    ],
-  },
+	branch,
+	clientId: "b42063f5-f145-4a9d-9b41-e5c8328deda7", // Get this from tina.io
+	token: "dba7d7108df1237d04333fb21e1108a51025750d", // Get this from tina.io
+	build: {
+		outputFolder: "admin",
+		publicFolder: "public",
+	},
+	media: {
+		tina: {
+			mediaRoot: "uploads",
+			publicFolder: "public",
+		},
+	},
+	schema: {
+		collections: [
+			{
+				name: "post",
+				label: "Posts",
+				path: "src/content",
+				format: "mdx",
+				fields: [
+					{
+						type: "string",
+						name: "title",
+						label: "Title",
+						isTitle: true,
+						required: true,
+					},
+					{
+						type: "string",
+						name: "description",
+						label: "Description",
+					},
+					{
+						type: "image",
+						name: "heroImage",
+						label: "Hero Image",
+					},
+					{
+						type: "datetime",
+						name: "pubDate",
+						label: "Publish Date",
+						ui: {
+							dateFormat: "DD MMMM YYYY",
+						},
+					},
+					{
+						type: "rich-text",
+						name: "body",
+						label: "Body",
+						isBody: true,
+						templates: [
+							{
+								name: "Counter",
+								label: "Counter",
+								fields: [
+									{
+										name: "variableExample",
+										label: "Variable Example",
+										type: "string",
+									},
+								],
+							},
+						],
+					},
+				],
+			},
+		],
+	},
 });
